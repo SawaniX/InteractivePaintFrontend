f28423c3233cab30cc3fea955e7ff173dbf04d7d
SawaniX <tomekwalburg@wp.pl>
2024-03-04 21:42:21 +0100

    Working sending and receiving webcam

--- src/Webcam.js
+++ src/Webcam.js
@@ -5,39 +5,19 @@ import "./Webcam.css";
 
 export default function ProcessVideoComponent() {
   const [websocket, setWebsocket] = useState(null);
-
-  useEffect(() => {
-    // Connect to the WebSocket endpoint
-    const ws = new WebSocket('ws://localhost:8000/virtual_paint');
-
-    // Set up event listeners
-    ws.onopen = () => {
-      console.log('WebSocket connection opened');
-    };
-
-    ws.onclose = () => {
-      console.log('WebSocket connection closed');
-    };
-
-    setWebsocket(ws);
-
-    // Clean up on component unmount
-    return () => {
-      if (ws.readyState === WebSocket.OPEN) {
-        ws.close();
-      }
-    };
-  }, []);
-
+  const [processedImage, setProcessedImage] = useState(null);
+  const webSocketRef = useRef();
   const video = useRef(null);
 
   useEffect(() => {
+    const webSocketRef = new WebSocket('ws://localhost:8000/virtual_paint');
+
     const sendVideo = (bytes) => {
       // Check if the WebSocket is open before sending the video stream
-      if (websocket && websocket.readyState === WebSocket.OPEN) {
+      if (webSocketRef && webSocketRef.readyState === WebSocket.OPEN) {
         // Assuming you have a function on the backend to handle the video stream
         console.log('Wysylam')
-        websocket.send(bytes);
+        webSocketRef.send(bytes);
       } else {
         console.error('WebSocket is not open');
       }
@@ -52,37 +32,50 @@ export default function ProcessVideoComponent() {
           } 
         });
 
-        if (stream && stream.getVideoTracks().length > 0) {
-          if (video.current) {
-            video.current.srcObject = stream;
-          }
-
-          setInterval(() => {
-            const canvas = document.createElement('canvas');
-            const context = canvas.getContext('2d');
-            canvas.width = video.current.videoWidth;
-            canvas.height = video.current.videoHeight;
-            context.drawImage(video.current, 0, 0, canvas.width, canvas.height);
-
-            // canvas.toBlob(blob => sendVideo(blob), 'image/jpeg');
-
-            const imageDataURL = canvas.toDataURL('image/jpeg');
-
-            sendVideo(imageDataURL)
-          }, 1000 / 1);
+        if (video.current) {
+          video.current.srcObject = stream;
         }
 
+        setInterval(() => {
+          const canvas = document.createElement('canvas');
+          const context = canvas.getContext('2d');
+          canvas.width = video.current.videoWidth;
+          canvas.height = video.current.videoHeight;
+          context.drawImage(video.current, 0, 0, canvas.width, canvas.height);
+
+          const imageDataURL = canvas.toDataURL('image/jpeg');
+
+          sendVideo(imageDataURL)
+        }, 1000 / 25);
       } catch (error) {
         console.error('Error accessing websam: ', error)
       }
     };
 
     enableWebcam();
-  }, [websocket]);
+
+    webSocketRef.onmessage = (event) => {
+      const processedImageData = event.data;
+      console.log(processedImageData)
+      // Update state with processed image data
+      setProcessedImage(processedImageData);
+    };
+
+    return () => {
+      if (webSocketRef.readyState === WebSocket.OPEN) {
+        webSocketRef.close();
+      }
+    };
+  }, []);
 
   return (
     <div>
+      lol
       <video ref={video} autoPlay playsInline />
+      <img
+        src={`data:image/jpeg;base64,${processedImage}`} // Adjust the MIME type based on your image format
+        alt="Processed"
+      />
     </div>
   );
 }
